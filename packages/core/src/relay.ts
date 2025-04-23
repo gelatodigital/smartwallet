abda3daa518e3f0ff39b8c925b4dc841bf9984c1
Muhammed Kadir Yücel <mky515@gmail.com>
2025-04-23 15:35:04 +0300

    chore: disable nodelete rule on biome

--- packages/core/src/relay.ts
+++ packages/core/src/relay.ts
@@ -14,7 +14,6 @@ export interface SponsoredCallRequest {
 
 export const sponsoredCall = async (request: SponsoredCallRequest): Promise<string> => {
   if (request.authorizationList && request.authorizationList.length > 0)
-    // biome-ignore lint/performance/noDelete: <explanation>
     delete request.authorizationList[0].v;
 
   const { taskId, message } = await fetch(`${GELATO_API}/relays/v2/sponsored-call-eip7702`, {
