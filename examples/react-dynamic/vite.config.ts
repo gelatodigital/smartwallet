ca96d812584beb93ca2ab5176d25f01224fa498e
Muhammed Kadir Yücel <mky515@gmail.com>
2025-04-23 21:31:00 +0300

    fix: react vite example missing packages

--- examples/react-dynamic/vite.config.ts
+++ examples/react-dynamic/vite.config.ts
@@ -1,10 +1,20 @@
+import path from 'node:path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@gelatomega/core': path.resolve(__dirname, '../../packages/core/src/index.ts'),
+      '@gelatomega/react-dynamic': path.resolve(
+        __dirname,
+        '../../packages/react-dynamic/src/index.ts',
+      ),
+    },
+  },
   optimizeDeps: {
-    exclude: ['@gelatomega/core', '@gelatomega/react-dynamic'],
+    include: ['@gelatomega/core', '@gelatomega/react-dynamic'],
   },
 });
