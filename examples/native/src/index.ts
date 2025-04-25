487a3ccd753cf39dd79416a46a9b44251c33fc54
Pedro Cruz <pedrocrvz@gmail.com>
2025-04-25 15:00:18 +0100

    chore: native example

---
+++ examples/native/src/index.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import { createMegaClient, native } from "@gelatomega/core";
+import { http, type Hex, createWalletClient } from "viem";
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+import { sepolia } from "viem/chains";
+
+const privateKey = (process.env.PRIVATE_KEY ?? generatePrivateKey()) as Hex;
+const account = privateKeyToAccount(privateKey);
+
+const client = createWalletClient({
+  account,
+  chain: sepolia,
+  transport: http()
+});
+
+createMegaClient(client)
+  .execute({
+    payment: native(),
+    calls: [
+      {
+        to: "0xa8851f5f279eD47a292f09CA2b6D40736a51788E",
+        data: "0xd09de08a",
+        value: 0n
+      }
+    ]
+  })
+  .then((id) => {
+    console.log(`Your Gelato id is: ${id}`);
+  });
