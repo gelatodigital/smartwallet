4653e1f5ca45ed01f891bb17e3f46529ae2a2278
Pedro Cruz <pedrocrvz@gmail.com>
2025-04-29 15:42:27 +0100

    chore: rollback viteconfig

--- examples/react-vite/vite.config.ts
+++ examples/react-vite/vite.config.ts
@@ -5,7 +5,32 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@gelatodigital/smartwallet": path.resolve(
+        __dirname,
+        "../../src/index.ts"
+      ),
+      "@gelatodigital/smartwallet-react-dynamic": path.resolve(
+        __dirname,
+        "../../plugins/react/dynamic/src/index.ts"
+      ),
+      "@gelatodigital/smartwallet-react-privy": path.resolve(
+        __dirname,
+        "../../plugins/react/privy/src/index.ts"
+      ),
+      "@gelatodigital/smartwallet-react-sdk": path.resolve(
+        __dirname,
+        "../../plugins/react/sdk/src/index.ts"
+      ),
+    },
+  },
   optimizeDeps: {
-    include: ["@gelatodigital/smartwallet", "@gelatodigital/smartwallet-react-sdk"]
-  }
+    include: [
+      "@gelatodigital/smartwallet",
+      "@gelatodigital/smartwallet-react-dynamic",
+      "@gelatodigital/smartwallet-react-privy",
+      "@gelatodigital/smartwallet-react-sdk",
+    ],
+  },
 });
