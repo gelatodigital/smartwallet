2d4fcd77549f32d389821976da08a573333ee6b9
Pedro Cruz <pedrocrvz@gmail.com>
2025-04-29 17:28:23 +0100

    chore: update delegation address and api endpoint

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import type { Address, Hex } from "viem";
 import { sepolia } from "viem/chains";
 
-const GELATO_API = "https://api.dev.gelato.digital";
+const GELATO_API = "https://api.gelato.digital";
 
 const EXECUTION_MODE = {
   default: "0x0100000000000000000000000000000000000000000000000000000000000000",
@@ -9,7 +9,7 @@ const EXECUTION_MODE = {
 };
 
 const DELEGATION_ADDRESSES: { [chainId: number]: Address } = {
-  [sepolia.id]: "0x515CCb8F7dE7ff2C70B148DC79827573e5b66ECC"
+  [sepolia.id]: "0x529C16817F74c5CcA41906Ab32F178e7da389b4b"
 };
 
 const NONCE_STORAGE_SLOT = "0xf2a7602a6b0fea467fdf81ac322504e60523f80eb506a1ca5e0f3e0d2ac70500";
