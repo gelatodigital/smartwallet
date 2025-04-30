c9a93f2966324f619b29406dee7bc58ae0944225
Pedro Cruz <pedrocrvz@gmail.com>
2025-04-30 10:46:41 +0100

    chore: lint

--- src/actions/internal/resolveERC20PaymentCall.ts
+++ src/actions/internal/resolveERC20PaymentCall.ts
@@ -2,11 +2,11 @@ import type { Account, Call, Chain, PublicActions, Transport, WalletClient } fro
 import { encodeFunctionData, formatUnits } from "viem";
 
 import { abi as erc20Abi } from "../../abis/erc20.js";
-import { getEstimatedFee } from "../../oracle/index.js";
 import { feeCollector } from "../../constants/index.js";
+import { getEstimatedFee } from "../../oracle/index.js";
+import type { ERC20Payment } from "../../payment/index.js";
 import { estimateGas } from "./estimateGas.js";
 import { verifyERC20Payment } from "./verifyERC20Payment.js";
-import type { ERC20Payment } from "../../payment/index.js";
 
 export async function resolveERC20PaymentCall<
   transport extends Transport = Transport,
