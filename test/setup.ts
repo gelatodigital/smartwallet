e3627abe55598d31c4cf3c79b75dcfb9568c6c70
Muhammed Kadir Yücel <mky515@gmail.com>
2025-05-02 16:27:28 +0300

    refactor: move mocking to beforeAll

--- test/setup.ts
+++ test/setup.ts
@@ -7,6 +7,11 @@ import { startAnvil, stopAnvil } from "./src/anvil.js";
 import { erc20Address } from "./src/constants.js";
 import { deployContracts } from "./src/contracts.js";
 
+import type { Hash } from "viem";
+import * as relay from "../src/relay/index.js";
+import { sponsorAccount } from "./src/account.js";
+import { wallet } from "./src/index.js";
+
 beforeAll(async () => {
   await startAnvil();
 
@@ -17,6 +22,43 @@ beforeAll(async () => {
   });
 
   console.log("Contracts deployed");
+
+  const sponsorClient = wallet.walletClient(sponsorAccount);
+
+  // Mock Relay calls locally
+  const mockSmartWalletCall = async (
+    parameters: relay.SmartWalletCallRequest
+  ): Promise<relay.GelatoResponse> => {
+    const hash = await sponsorClient.sendTransaction({
+      authorizationList: parameters.authorizationList,
+      data: parameters.data as Hash,
+      to: parameters.target as Hash
+    });
+
+    return {
+      id: "mock-task-id",
+      wait: async () => hash,
+      on: () => () => {}
+    };
+  };
+  const mockSponsoredCall = async (
+    parameters: relay.SponsoredCallRequest
+  ): Promise<relay.GelatoResponse> => {
+    const hash = await sponsorClient.sendTransaction({
+      authorizationList: parameters.authorizationList,
+      data: parameters.data as Hash,
+      to: parameters.target as Hash
+    });
+
+    return {
+      id: "mock-task-id",
+      wait: async () => hash,
+      on: () => () => {}
+    };
+  };
+
+  vi.spyOn(relay, "smartWalletCall").mockImplementation(mockSmartWalletCall);
+  vi.spyOn(relay, "sponsoredCall").mockImplementation(mockSponsoredCall);
 });
 
 afterAll(async () => {
