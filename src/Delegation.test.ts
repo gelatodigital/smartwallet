1611a31caa1258ba8475ef8f6873c8161afd1cf1
Muhammed Kadir Yücel <mky515@gmail.com>
2025-05-02 16:30:48 +0300

    chore: delegation testing

---
+++ src/Delegation.test.ts
@@ -0,0 +1,127 @@
+import { describe, expect, test } from "vitest";
+
+import { constants, SimpleERC20, account, utils, wallet } from "../test/src/index.js";
+import { createGelatoSmartWalletClient, erc20, native, sponsored } from "./index.js";
+
+describe("Initial Delegation Test", () => {
+  test("Delegate with native payment", async () => {
+    const { account: testAccount } = await account.getAccount();
+
+    const walletClient = wallet.walletClient(testAccount);
+
+    const gelatoClient = createGelatoSmartWalletClient(walletClient);
+    const balanceInitial = await gelatoClient.getBalance({
+      address: gelatoClient.account.address
+    });
+
+    await gelatoClient.execute({
+      payment: native(),
+      calls: constants.testCalls
+    });
+
+    await utils.waitBlockTime();
+
+    const code = await gelatoClient.getCode({
+      address: gelatoClient.account.address
+    });
+
+    const balanceFinal = await gelatoClient.getBalance({
+      address: gelatoClient.account.address
+    });
+
+    expect(code).toBe(constants.delegationCode(gelatoClient.chain.id).toLowerCase());
+    expect(balanceFinal).toBeLessThan(balanceInitial);
+  });
+
+  test("Delegate with ERC20 payment", async () => {
+    const { account: testAccount } = await account.getAccount();
+
+    const walletClient = wallet.walletClient(testAccount);
+
+    const gelatoClient = createGelatoSmartWalletClient(walletClient);
+    const balanceInitial = await gelatoClient.getBalance({
+      address: gelatoClient.account.address
+    });
+
+    const erc20BalanceInitial = await gelatoClient.readContract({
+      address: constants.erc20Address(),
+      abi: SimpleERC20.abi,
+      functionName: "balanceOf",
+      args: [gelatoClient.account.address]
+    });
+
+    await gelatoClient.execute({
+      payment: erc20(constants.erc20Address()),
+      calls: constants.testCalls
+    });
+
+    await utils.waitBlockTime();
+
+    const code = await gelatoClient.getCode({
+      address: gelatoClient.account.address
+    });
+
+    const balanceFinal = await gelatoClient.getBalance({
+      address: gelatoClient.account.address
+    });
+
+    const erc20BalanceFinal = await gelatoClient.readContract({
+      address: constants.erc20Address(),
+      abi: SimpleERC20.abi,
+      functionName: "balanceOf",
+      args: [gelatoClient.account.address]
+    });
+
+    expect(code).toBe(constants.delegationCode(gelatoClient.chain.id).toLowerCase());
+    expect(balanceFinal).toBe(balanceInitial);
+    expect(erc20BalanceFinal).toBeLessThan(erc20BalanceInitial);
+  });
+
+  test("Delegate with sponsor payment", async () => {
+    const { account: testAccount } = await account.getAccount();
+    const walletClient = wallet.walletClient(testAccount);
+
+    const gelatoClient = createGelatoSmartWalletClient(walletClient);
+    const balanceInitial = await gelatoClient.getBalance({
+      address: gelatoClient.account.address
+    });
+    const erc20BalanceInitial = await gelatoClient.readContract({
+      address: constants.erc20Address(),
+      abi: SimpleERC20.abi,
+      functionName: "balanceOf",
+      args: [gelatoClient.account.address]
+    });
+    const sponsorBalanceInitial = await gelatoClient.getBalance({
+      address: account.sponsorAccount.address
+    });
+
+    await gelatoClient.execute({
+      payment: sponsored("random-key"),
+      calls: constants.testCalls
+    });
+
+    await utils.waitBlockTime();
+
+    const code = await gelatoClient.getCode({
+      address: gelatoClient.account.address
+    });
+
+    const balanceFinal = await gelatoClient.getBalance({
+      address: gelatoClient.account.address
+    });
+    const sponsorBalanceFinal = await gelatoClient.getBalance({
+      address: account.sponsorAccount.address
+    });
+    const erc20BalanceFinal = await gelatoClient.readContract({
+      address: constants.erc20Address(),
+      abi: SimpleERC20.abi,
+      functionName: "balanceOf",
+      args: [gelatoClient.account.address]
+    });
+
+    expect(code).toBe(constants.delegationCode(gelatoClient.chain.id).toLowerCase());
+    expect(balanceFinal).toBe(balanceInitial);
+    expect(erc20BalanceFinal).toBe(erc20BalanceInitial);
+    expect(sponsorBalanceFinal).toBeLessThan(sponsorBalanceInitial);
+  });
+});
