71e3eeac95f4eb82e2e404942e8b0d9f0e69b1da
Ben Price <ben.jules.price@gmail.com>
2025-05-06 14:59:56 +0100

    feat: improve get nonce

--- src/actions/internal/getOpData.ts
+++ src/actions/internal/getOpData.ts
@@ -1,6 +1,6 @@
-import { type Account, type Call, type Chain, type Transport, hexToBigInt } from "viem";
+import { type Account, type Call, type Chain, type Transport } from "viem";
 
-import { nonceStorageSlot } from "../../constants/index.js";
+import { abi as accountAbi } from "../../abis/account.js";
 import { serializeTypedData } from "../../utils/eip712.js";
 import type { GelatoWalletClient } from "../index.js";
 
@@ -9,16 +9,11 @@ export async function getOpData<
   chain extends Chain = Chain,
   account extends Account = Account
 >(client: GelatoWalletClient<transport, chain, account>, calls: Call[]) {
-  const nonceHex = await client.getStorageAt({
+  const nonce = await client.readContract({
     address: client.account.address,
-    slot: nonceStorageSlot()
+    abi: accountAbi,
+    functionName: "getNonce"
   });
-
-  if (!nonceHex) {
-    throw new Error("Failed to get nonce");
-  }
-
-  const nonce = hexToBigInt(nonceHex);
 
   return serializeTypedData(client.chain.id, client.account.address, "opData", calls, nonce);
 }
