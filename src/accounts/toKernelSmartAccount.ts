3e6f882b7938ed15e171249b0b753f0beccd76d3
Muhammed Kadir Yücel <mky515@gmail.com>
2025-05-20 17:32:53 +0300

    refactor: use self implementation of encode and decode

--- src/accounts/toKernelSmartAccount.ts
+++ src/accounts/toKernelSmartAccount.ts
@@ -1,17 +1,19 @@
-import { decode7579Calls, encode7579Calls } from "permissionless";
 import type { Address, Call, Hex, Prettify, TypedData, TypedDataDefinition } from "viem";
 import {
-  BaseError,
   concatHex,
+  decodeAbiParameters,
   decodeFunctionData,
   domainSeparator,
   encodeAbiParameters,
   encodeFunctionData,
+  encodePacked,
   getTypesForEIP712Domain,
   hashMessage,
   hashTypedData,
   keccak256,
+  slice,
   stringToHex,
+  toBytes,
   toHex,
   validateTypedData,
   zeroAddress
@@ -25,19 +27,15 @@ import {
   toSmartAccount
 } from "viem/account-abstraction";
 import type { PrivateKeyAccount } from "viem/accounts";
-import { getCode, readContract } from "viem/actions";
+import { getChainId, getCode, readContract } from "viem/actions";
 import { baseSepolia, sepolia } from "viem/chains";
 import { getAction } from "viem/utils";
 
 import { getSenderAddress } from "permissionless/actions";
 import { delegationAbi as abi } from "../abis/delegation.js";
 
-/* TODO
-  1- Handle if ZeroDev Kernel is deployed 
-  2- If deployed just utilized that like `permissionless.js`
-  3- If not deployed fallback to EIP-7702 delegation case and handle it like GelatoAccount
-  4- Handle encode/decode functions (use permissionless for now, then migrate to us only)
-*/
+type FactoryArgsHandler = () => Promise<{ factory: Hex; factoryData: Hex }>;
+type Authorization = { account: PrivateKeyAccount; address: Address };
 
 export type ToKernelSmartAccountParameters = {
   client: KernelSmartAccountImplementation["client"];
@@ -57,35 +55,15 @@ export type KernelSmartAccountImplementation = SmartAccountImplementation<
   { abi: typeof abi; owner: PrivateKeyAccount }
 >;
 
-// for kernel version >=0.3.1
-const KERNEL_ECDSA_VALIDATOR_KEY = BigInt("0x0000845ADb2C711129d4f3966735eD98a9F09fC4cE570000");
-const KERNEL_ECDSA_VALIDATOR: Hex = "0x845ADb2C711129d4f3966735eD98a9F09fC4cE57";
-const KERNEL_FACTORY_ADDRESS: Hex = "0xE30c76Dc9eCF1c19F6Fec070674E1b4eFfE069FA";
-const KERNEL_META_FACTORY_ADDRESS: Hex = "0xd703aaE79538628d27099B8c4f621bE4CCd142d5";
-
-const KERNEL_V3_3_DELEGATION_ADDRESSES: { [chainId: number]: Address } = {
-  [sepolia.id]: "0xd6CEDDe84be40893d153Be9d467CD6aD37875b28",
-  [baseSepolia.id]: "0xd6CEDDe84be40893d153Be9d467CD6aD37875b28"
-  // not deployed on ink sepolia
-};
-
-const delegationAddress = (chainId: number) => {
-  const address = KERNEL_V3_3_DELEGATION_ADDRESSES[chainId];
-  if (!address) {
-    throw new Error(`Unsupported chain: ${chainId}`);
-  }
-  return address;
-};
-
 const getInitializationData = ({
   validatorData
 }: {
   validatorData: Hex;
 }) => {
   return encodeFunctionData({
-    abi: KernelV3_1AccountAbi,
+    abi: KernelV3AccountAbi,
     functionName: "initialize",
-    args: [concatHex(["0x01", KERNEL_ECDSA_VALIDATOR]), zeroAddress, validatorData, "0x", []]
+    args: [kernelV3_3_EcdsaRootIdentifier(), zeroAddress, validatorData, "0x", []]
   });
 };
 
@@ -103,8 +81,6 @@ const getAccountInitCode = async ({
     validatorData
   });
 
-  // Build the account init code
-
   if (!useMetaFactory) {
     return encodeFunctionData({
       abi: KernelV3FactoryAbi,
@@ -116,7 +92,7 @@ const getAccountInitCode = async ({
   return encodeFunctionData({
     abi: KernelV3MetaFactoryDeployWithFactoryAbi,
     functionName: "deployWithFactory",
-    args: [KERNEL_FACTORY_ADDRESS, initializationData, toHex(index, { size: 32 })]
+    args: [KERNEL_V3_3_FACTORY_ADDRESS, initializationData, toHex(index, { size: 32 })]
   });
 };
 
@@ -131,39 +107,15 @@ export async function toKernelSmartAccount(
     version: "0.7"
   } as const;
 
-  let isDelegated = false;
+  let isDelegated = false; // When 7702+4337 is used
+  let chainId: number;
 
-  const getAccountAddressAndFactoryArgs = async (parameters: {
-    accountAddress: Address;
-    getFactoryArgs: () => Promise<{ factory: Hex; factoryData: Hex }>;
-  }): Promise<{
-    accountAddress: Address;
-    getFactoryArgs: () => Promise<{ factory: Hex; factoryData: Hex }>;
-    authorization?: { account: PrivateKeyAccount; address: Address };
-  }> => {
-    const { accountAddress, getFactoryArgs } = parameters;
-
-    const code = await getAction(
-      client,
-      getCode,
-      "getCode"
-    )({
-      address: accountAddress
-    });
-
-    const deployed = Boolean(code);
-
-    if (deployed) {
-      return { accountAddress, getFactoryArgs, authorization: undefined };
-    }
-
-    isDelegated = true;
-    return {
-      accountAddress: owner.address,
-      getFactoryArgs: get7702FactoryArgs,
-      // biome-ignore lint/style/noNonNullAssertion: <explanation>
-      authorization: { account: owner, address: delegationAddress(client.chain!.id) }
-    };
+  const getMemoizedChainId = async () => {
+    if (chainId) return chainId;
+    chainId = client.chain
+      ? client.chain.id
+      : await getAction(client, getChainId, "getChainId")({});
+    return chainId;
   };
 
   const generateInitCode = async (_useMetaFactory: boolean) => {
@@ -174,18 +126,80 @@ export async function toKernelSmartAccount(
     });
   };
 
-  const get7702FactoryArgs = async (): Promise<{ factory: Hex; factoryData: Hex }> => {
-    return { factory: "0x7702", factoryData: "0x" };
-  };
-
   const getFactoryArgsFunc = (_useMetaFactory: boolean) => async () => {
     return {
-      factory: _useMetaFactory === false ? KERNEL_FACTORY_ADDRESS : KERNEL_META_FACTORY_ADDRESS,
+      factory:
+        _useMetaFactory === false ? KERNEL_V3_3_FACTORY_ADDRESS : KERNEL_V3_3_META_FACTORY_ADDRESS,
       factoryData: await generateInitCode(_useMetaFactory)
     };
   };
 
+  const wrappedMessage = async (hash: Hex) => {
+    const chainId = await getMemoizedChainId();
+
+    const _domainSeparator = domainSeparator({
+      domain: {
+        name: "Kernel",
+        version: "0.3.3",
+        chainId,
+        verifyingContract: accountAddress
+      }
+    });
+
+    const wrappedMessageHash = keccak256(
+      encodeAbiParameters(
+        [{ type: "bytes32" }, { type: "bytes32" }],
+        [keccak256(stringToHex("Kernel(bytes32 hash)")), hash]
+      )
+    );
+
+    const digest = keccak256(concatHex(["0x1901", _domainSeparator, wrappedMessageHash]));
+
+    return digest;
+  };
+
   const { accountAddress, getFactoryArgs, authorization } = await (async () => {
+    const get7702FactoryArgs = async (): Promise<{ factory: Hex; factoryData: Hex }> => {
+      return { factory: "0x7702", factoryData: "0x" };
+    };
+
+    const getAccountAddressAndFactoryArgs = async (parameters: {
+      accountAddress: Address;
+      getFactoryArgs: FactoryArgsHandler;
+    }): Promise<{
+      accountAddress: Address;
+      getFactoryArgs: FactoryArgsHandler;
+      authorization?: Authorization;
+    }> => {
+      const { accountAddress, getFactoryArgs } = parameters;
+
+      const code = await getAction(
+        client,
+        getCode,
+        "getCode"
+      )({
+        address: accountAddress
+      });
+
+      const deployed = Boolean(code);
+
+      if (deployed) {
+        return {
+          accountAddress,
+          getFactoryArgs
+        };
+      }
+
+      const chainId = await getMemoizedChainId();
+      isDelegated = true;
+
+      return {
+        accountAddress: owner.address,
+        getFactoryArgs: get7702FactoryArgs,
+        authorization: { account: owner, address: delegationAddress(chainId) }
+      };
+    };
+
     let getFactoryArgs = getFactoryArgsFunc(useMetaFactory === "optional" ? true : useMetaFactory);
 
     if (address && useMetaFactory !== "optional") {
@@ -223,23 +237,149 @@ export async function toKernelSmartAccount(
     getFactoryArgs,
     abi,
     client,
-    extend: { abi, owner }, // not removing abi from here as this will be a breaking change
+    extend: { abi, owner },
     entryPoint,
 
     async decodeCalls(data) {
-      return decode7579Calls(data).callData;
+      const decoded = decodeFunctionData({
+        abi: executeAbi,
+        data
+      });
+
+      const encodedMode = decoded.args[0];
+      const executionCalldata = decoded.args[1];
+
+      const callType = slice(encodedMode, 0, 1);
+
+      // batchcall
+      if (callType === "0x01") {
+        const [calls] = decodeAbiParameters(
+          [
+            {
+              name: "executionBatch",
+              type: "tuple[]",
+              components: [
+                {
+                  name: "target",
+                  type: "address"
+                },
+                {
+                  name: "value",
+                  type: "uint256"
+                },
+                {
+                  name: "callData",
+                  type: "bytes"
+                }
+              ]
+            }
+          ],
+          executionCalldata
+        );
+
+        return calls.map((call) => ({
+          to: call.target,
+          value: call.value,
+          data: call.callData
+        }));
+      }
+
+      const decodedCall = decodeAbiParameters(
+        [
+          {
+            type: "tuple",
+            components: [
+              { type: "address", name: "to" },
+              { type: "uint256", name: "value" },
+              { type: "bytes", name: "data" }
+            ]
+          }
+        ],
+        executionCalldata
+      ) as [Call];
+
+      return decodedCall;
     },
 
     async encodeCalls(calls) {
-      return encode7579Calls({
-        mode: {
-          type: calls.length > 1 ? "batchcall" : "call",
-          revertOnError: false,
-          selector: "0x",
-          context: "0x"
-        },
-        callData: calls
-      });
+      const mode = {
+        type: calls.length > 1 ? "batchcall" : "call",
+        revertOnError: false,
+        selector: "0x",
+        context: "0x"
+      };
+
+      if (calls.length === 0) {
+        throw new Error("Encode: No calls");
+      }
+
+      const parsedCallType =
+        mode.type === "call" ? "0x00" : mode.type === "batchcall" ? "0x01" : "0xff";
+      const encodedMode = encodePacked(
+        ["bytes1", "bytes1", "bytes4", "bytes4", "bytes22"],
+        [
+          toHex(toBytes(parsedCallType, { size: 1 })),
+          toHex(toBytes(mode.revertOnError ? "0x01" : "0x00", { size: 1 })),
+          toHex(toBytes("0x0", { size: 4 })),
+          toHex(toBytes(mode.selector ?? "0x", { size: 4 })),
+          toHex(toBytes(mode.context ?? "0x", { size: 22 }))
+        ]
+      );
+
+      if (calls.length === 1) {
+        const call = calls[0];
+        return encodeFunctionData({
+          abi: executeAbi,
+          functionName: "execute",
+          args: [
+            encodedMode,
+            concatHex([call.to, toHex(call.value ?? 0n, { size: 32 }), call.data ?? "0x"])
+          ]
+        });
+      }
+
+      if (calls.length > 1) {
+        return encodeFunctionData({
+          abi: executeAbi,
+          functionName: "execute",
+          args: [
+            encodedMode,
+            encodeAbiParameters(
+              [
+                {
+                  name: "executionBatch",
+                  type: "tuple[]",
+                  components: [
+                    {
+                      name: "target",
+                      type: "address"
+                    },
+                    {
+                      name: "value",
+                      type: "uint256"
+                    },
+                    {
+                      name: "callData",
+                      type: "bytes"
+                    }
+                  ]
+                }
+              ],
+              [
+                calls.map((arg) => {
+                  return {
+                    target: arg.to,
+                    value: arg.value ?? 0n,
+                    callData: arg.data ?? "0x"
+                  };
+                })
+              ]
+            )
+          ]
+        });
+      }
+
+      throw new Error("Encode: Unsupported call type");
     },
 
     async getNonce(_parameters?: { key?: bigint }): Promise<bigint> {
@@ -247,7 +387,7 @@ export async function toKernelSmartAccount(
         abi: entryPoint07Abi,
         address: entryPoint07Address,
         functionName: "getNonce",
-        args: [accountAddress, KERNEL_ECDSA_VALIDATOR_KEY]
+        args: [accountAddress, KERNEL_V3_3_ECDSA_VALIDATOR_KEY]
       });
     },
 
@@ -268,30 +408,13 @@ export async function toKernelSmartAccount(
         return await owner.signMessage({ message });
       }
 
-      // biome-ignore lint/style/noNonNullAssertion: <explanation>
-      const chainId = client.chain!.id;
+      const wrapped = await wrappedMessage(hashMessage(message));
 
-      const _domainSeparator = domainSeparator({
-        domain: {
-          name: "Kernel",
-          version: "0.3.3",
-          chainId,
-          verifyingContract: accountAddress
-        }
+      const signature = await owner.signMessage({
+        message: { raw: wrapped }
       });
 
-      const wrappedMessageHash = keccak256(
-        encodeAbiParameters(
-          [{ type: "bytes32" }, { type: "bytes32" }],
-          [keccak256(stringToHex("Kernel(bytes32 hash)")), hashMessage(message)]
-        )
-      );
-
-      const digest = keccak256(concatHex(["0x1901", _domainSeparator, wrappedMessageHash]));
-
-      return owner.signMessage({
-        message: { raw: digest }
-      });
+      return concatHex([kernelV3_3_EcdsaRootIdentifier(), signature]);
     },
 
     async signTypedData(parameters) {
@@ -327,35 +450,17 @@ export async function toKernelSmartAccount(
 
       const typedHash = hashTypedData({ message, primaryType, types, domain });
 
-      // biome-ignore lint/style/noNonNullAssertion: <explanation>
-      const chainId = client.chain!.id;
+      const wrapped = await wrappedMessage(typedHash);
 
-      const _domainSeparator = domainSeparator({
-        domain: {
-          name: "Kernel",
-          version: "0.3.3",
-          chainId,
-          verifyingContract: accountAddress
-        }
+      const signature = await owner.signMessage({
+        message: { raw: wrapped }
       });
 
-      const wrappedMessageHash = keccak256(
-        encodeAbiParameters(
-          [{ type: "bytes32" }, { type: "bytes32" }],
-          [keccak256(stringToHex("Kernel(bytes32 hash)")), typedHash]
-        )
-      );
-
-      const digest = keccak256(concatHex(["0x1901", _domainSeparator, wrappedMessageHash]));
-
-      return owner.signMessage({
-        message: { raw: digest }
-      });
+      return concatHex([kernelV3_3_EcdsaRootIdentifier(), signature]);
     },
 
     async signUserOperation(parameters) {
-      // biome-ignore lint/style/noNonNullAssertion: <explanation>
-      const { chainId = client.chain!.id, ...userOperation } = parameters;
+      const { chainId = await getMemoizedChainId(), ...userOperation } = parameters;
 
       const hash = getUserOperationHash({
         userOperation: {
@@ -377,7 +482,31 @@ export async function toKernelSmartAccount(
   });
 }
 
-export const KernelV3_1AccountAbi = [
+/// Constants
+
+const KERNEL_V3_3_ECDSA_VALIDATOR_KEY = BigInt(
+  "0x0000845ADb2C711129d4f3966735eD98a9F09fC4cE570000"
+);
+const KERNEL_V3_3_ECDSA_VALIDATOR: Hex = "0x845ADb2C711129d4f3966735eD98a9F09fC4cE57";
+const KERNEL_V3_3_FACTORY_ADDRESS: Hex = "0xE30c76Dc9eCF1c19F6Fec070674E1b4eFfE069FA";
+const KERNEL_V3_3_META_FACTORY_ADDRESS: Hex = "0xd703aaE79538628d27099B8c4f621bE4CCd142d5";
+const KERNEL_V3_3_DELEGATION_ADDRESSES: { [chainId: number]: Address } = {
+  [sepolia.id]: "0xd6CEDDe84be40893d153Be9d467CD6aD37875b28",
+  [baseSepolia.id]: "0xd6CEDDe84be40893d153Be9d467CD6aD37875b28"
+  // not deployed on ink sepolia
+};
+
+const delegationAddress = (chainId: number) => {
+  const address = KERNEL_V3_3_DELEGATION_ADDRESSES[chainId];
+  if (!address) {
+    throw new Error(`Unsupported chain: ${chainId}`);
+  }
+  return address;
+};
+
+const kernelV3_3_EcdsaRootIdentifier = () => concatHex(["0x01", KERNEL_V3_3_ECDSA_VALIDATOR]);
+
+export const KernelV3AccountAbi = [
   {
     type: "function",
     name: "initialize",
@@ -424,6 +553,27 @@ const KernelV3MetaFactoryDeployWithFactoryAbi = [
       { name: "salt", type: "bytes32", internalType: "bytes32" }
     ],
     outputs: [{ name: "", type: "address", internalType: "address" }],
+    stateMutability: "payable"
+  }
+] as const;
+
+const executeAbi = [
+  {
+    type: "function",
+    name: "execute",
+    inputs: [
+      {
+        name: "execMode",
+        type: "bytes32",
+        internalType: "ExecMode"
+      },
+      {
+        name: "executionCalldata",
+        type: "bytes",
+        internalType: "bytes"
+      }
+    ],
+    outputs: [],
     stateMutability: "payable"
   }
 ] as const;
