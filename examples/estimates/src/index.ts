f4178161c4a86e582243f4ecb53f5dcd2f948662
Muhammed Kadir Yücel <mky515@gmail.com>
2025-05-20 18:00:01 +0300

    chore: update examples

--- examples/estimates/src/index.ts
+++ examples/estimates/src/index.ts
@@ -1,6 +1,10 @@
-import { createGelatoSmartWalletClient, sponsored } from "@gelatonetwork/smartwallet";
 import "dotenv/config";
-import { http, type Hex, createWalletClient, formatUnits } from "viem";
+import {
+  createGelatoSmartWalletClient,
+  sponsored,
+  toGelatoSmartAccount
+} from "@gelatonetwork/smartwallet";
+import { http, type Hex, createPublicClient, createWalletClient, formatEther } from "viem";
 import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
 import { sepolia } from "viem/chains";
 
@@ -11,27 +15,39 @@ if (!sponsorApiKey) {
 }
 
 const privateKey = (process.env.PRIVATE_KEY ?? generatePrivateKey()) as Hex;
-const account = privateKeyToAccount(privateKey);
+const owner = privateKeyToAccount(privateKey);
 
-const client = createWalletClient({
-  account,
+const publicClient = createPublicClient({
   chain: sepolia,
   transport: http()
 });
 
-createGelatoSmartWalletClient(client, { apiKey: sponsorApiKey })
-  .estimate({
-    payment: sponsored(sponsorApiKey),
-    calls: [
-      {
-        to: "0xa8851f5f279eD47a292f09CA2b6D40736a51788E",
-        data: "0xd09de08a",
-        value: 0n
-      }
-    ]
-  })
-  .then(async ({ fee, gas }) => {
-    console.log(`Estimated fee: ${formatUnits(BigInt(fee.estimatedFee), fee.decimals)} ETH`);
-    console.log(`Estimated gas: ${gas} GAS`);
-    process.exit(0);
+(async () => {
+  const account = await toGelatoSmartAccount({
+    owner,
+    client: publicClient
   });
+
+  const client = createWalletClient({
+    account,
+    chain: sepolia,
+    transport: http()
+  });
+
+  createGelatoSmartWalletClient(client, { apiKey: sponsorApiKey })
+    .estimate({
+      payment: sponsored(sponsorApiKey),
+      calls: [
+        {
+          to: "0xa8851f5f279eD47a292f09CA2b6D40736a51788E",
+          data: "0xd09de08a",
+          value: 0n
+        }
+      ]
+    })
+    .then(async ({ estimatedFee, estimatedGas }) => {
+      console.log(`Estimated fee: ${formatEther(estimatedFee)} ETH`);
+      console.log(`Estimated gas: ${estimatedGas} GAS`);
+      process.exit(0);
+    });
+})();
