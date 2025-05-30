e2910525aa4249d4e6a270a9bff8ed67c798edca
Pedro Cruz <pedrocrvz@gmail.com>
2025-05-30 16:45:15 +0100

    feat: remove entrypoint v0.6 support

--- src/accounts/safe/constants.ts
+++ src/accounts/safe/constants.ts
@@ -4,6 +4,8 @@ import type { Address } from "viem";
 
 export type SafeVersion = "1.4.1";
 
+export type SupportedEntryPointVersions = "0.7";
+
 export const multiSendAbi = [
   {
     inputs: [
@@ -119,23 +121,21 @@ export const createProxyWithNonceAbi = [
   }
 ] as const;
 
-export const EIP712_SAFE_OPERATION_TYPE_V06 = {
-  SafeOp: [
-    { type: "address", name: "safe" },
-    { type: "uint256", name: "nonce" },
-    { type: "bytes", name: "initCode" },
-    { type: "bytes", name: "callData" },
-    { type: "uint256", name: "callGasLimit" },
-    { type: "uint256", name: "verificationGasLimit" },
-    { type: "uint256", name: "preVerificationGas" },
-    { type: "uint256", name: "maxFeePerGas" },
-    { type: "uint256", name: "maxPriorityFeePerGas" },
-    { type: "bytes", name: "paymasterAndData" },
-    { type: "uint48", name: "validAfter" },
-    { type: "uint48", name: "validUntil" },
-    { type: "address", name: "entryPoint" }
-  ]
-};
+export const proxyCreationCodeAbi = [
+  {
+    inputs: [],
+    name: "proxyCreationCode",
+    outputs: [
+      {
+        internalType: "bytes",
+        name: "",
+        type: "bytes"
+      }
+    ],
+    stateMutability: "pure",
+    type: "function"
+  }
+] as const;
 
 export const EIP712_SAFE_OPERATION_TYPE_V07 = {
   SafeOp: [
@@ -157,7 +157,7 @@ export const EIP712_SAFE_OPERATION_TYPE_V07 = {
 
 export const SAFE_VERSION_TO_ADDRESSES_MAP: {
   [key in SafeVersion]: {
-    [key in "0.6" | "0.7"]: {
+    [key in "0.7"]: {
       SAFE_MODULE_SETUP_ADDRESS: Address;
       SAFE_4337_MODULE_ADDRESS: Address;
       SAFE_PROXY_FACTORY_ADDRESS: Address;
@@ -168,14 +168,6 @@ export const SAFE_VERSION_TO_ADDRESSES_MAP: {
   };
 } = {
   "1.4.1": {
-    "0.6": {
-      SAFE_MODULE_SETUP_ADDRESS: "0x8EcD4ec46D4D2a6B64fE960B3D64e8B94B2234eb",
-      SAFE_4337_MODULE_ADDRESS: "0xa581c4A4DB7175302464fF3C06380BC3270b4037",
-      SAFE_PROXY_FACTORY_ADDRESS: "0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67",
-      SAFE_SINGLETON_ADDRESS: "0x41675C099F32341bf84BFc5382aF534df5C7461a",
-      MULTI_SEND_ADDRESS: "0x38869bf66a61cF6bDB996A6aE40D5853Fd43B526",
-      MULTI_SEND_CALL_ONLY_ADDRESS: "0x9641d764fc13c8B624c04430C7356C1C7C8102e2"
-    },
     "0.7": {
       SAFE_MODULE_SETUP_ADDRESS: "0x2dd68b007B46fBe91B9A7c3EDa5A7a1063cB5b47",
       SAFE_4337_MODULE_ADDRESS: "0x75cf11467937ce3F2f357CE24ffc3DBF8fD5c226",
