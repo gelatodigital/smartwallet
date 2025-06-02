c5fba2dd55474e308a12f0ae395eff5a48941721
Muhammed Kadir Yücel <mky515@gmail.com>
2025-06-02 17:02:26 +0300

    feat: add sendTransaction

--- plugins/react/wagmi/src/index.ts
+++ plugins/react/wagmi/src/index.ts
@@ -3,4 +3,10 @@ export {
   type GelatoSmartWalletProviderProps
 } from "./context.js";
 
+export {
+  useSendTransaction,
+  type UseSendTransactionParameters,
+  type UseSendTransactionReturnType
+} from "./hooks/useSendTransaction.js";
+
 export { useGelatoSmartWalletClient } from "./hooks/useGelatoSmartWalletClient.js";
