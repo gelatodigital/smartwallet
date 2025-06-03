4cb7411da7bc59bea31393b31b68945536541e89
brandon.cs <brandonchuah.sl@gmail.com>
2025-06-03 16:51:38 +0800

    chore: update test to run on sepolia (#68)
    * chore: update test to run on sepolia
    
    * chore: env variable
    
    * chore: gh secrets
    
    * chore: increase test timeout
    
    * chore: remove unused
    
    * chore: remove unused

--- test/src/utils.ts
+++
@@ -1,4 +0,0 @@
-import { blockTime } from "./constants.js";
-
-const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-export const waitBlockTime = (numberBlocks = 1) => wait(blockTime * numberBlocks * 1000);
