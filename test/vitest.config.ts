74ad7c967957f03106a42c34697f27717f9cb7f3
brandon.cs <brandonchuah.sl@gmail.com>
2025-06-03 17:20:59 +0800

    chore: single thread (#72)

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -20,7 +20,9 @@ export default defineConfig(({ mode }) => {
         join(join(dirname(path), "_snapshots"), `${basename(path)}${ext}`),
       setupFiles: [join(__dirname, "./setup.ts")],
       testTimeout: 60_000,
-      env: loadEnv(mode, process.cwd(), "")
+      env: loadEnv(mode, process.cwd(), ""),
+      pool: "threads",
+      poolOptions: { threads: { singleThread: true } }
     }
   };
 });
