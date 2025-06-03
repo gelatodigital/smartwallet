ecc7372e813eff080b2469fb223c88c8b6a78b4f
brandon.cs <brandonchuah.sl@gmail.com>
2025-06-03 16:51:38 +0800

    chore: update test to run on sepolia (#68)
    * chore: update test to run on sepolia
    
    * chore: env variable
    
    * chore: gh secrets
    
    * chore: increase test timeout
    
    * chore: remove unused
    
    * chore: remove unused

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -20,9 +20,7 @@ export default defineConfig(({ mode }) => {
         join(join(dirname(path), "_snapshots"), `${basename(path)}${ext}`),
       setupFiles: [join(__dirname, "./setup.ts")],
       testTimeout: 60_000,
-      env: loadEnv(mode, process.cwd(), ""),
-      pool: "threads",
-      poolOptions: { threads: { singleThread: true } }
+      env: loadEnv(mode, process.cwd(), "")
     }
   };
 });
