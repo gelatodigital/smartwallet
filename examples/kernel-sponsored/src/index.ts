56c7b54db5e11b0bcbe8f35c96dae8ca6558fc89
Ben Price <ben.jules.price@gmail.com>
2025-06-10 22:18:44 +0200

    chore: remove unused import

--- examples/kernel-sponsored/src/index.ts
+++ examples/kernel-sponsored/src/index.ts
@@ -2,12 +2,12 @@ import "dotenv/config";
 import {
   type GelatoTaskStatus,
   createGelatoSmartWalletClient,
-  sponsored
+  sponsored,
 } from "@gelatonetwork/smartwallet";
 import { kernel } from "@gelatonetwork/smartwallet/accounts";
 import { http, type Hex, createPublicClient, createWalletClient } from "viem";
 import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
-import { baseSepolia, sepolia } from "viem/chains";
+import { baseSepolia } from "viem/chains";
 
 const sponsorApiKey = process.env.SPONSOR_API_KEY;
 
@@ -20,14 +20,14 @@ const owner = privateKeyToAccount(privateKey);
 
 const publicClient = createPublicClient({
   chain: baseSepolia,
-  transport: http()
+  transport: http(),
 });
 
 (async () => {
   const account = await kernel({
     owner,
     client: publicClient,
-    eip7702: false
+    eip7702: false,
   });
 
   console.log("Account address:", account.address);
@@ -35,10 +35,12 @@ const publicClient = createPublicClient({
   const client = createWalletClient({
     account,
     chain: baseSepolia,
-    transport: http()
+    transport: http(),
   });
 
-  const swc = await createGelatoSmartWalletClient(client, { apiKey: sponsorApiKey });
+  const swc = await createGelatoSmartWalletClient(client, {
+    apiKey: sponsorApiKey,
+  });
 
   const response = await swc.execute({
     payment: sponsored(sponsorApiKey),
@@ -46,9 +48,9 @@ const publicClient = createPublicClient({
       {
         to: "0xa8851f5f279eD47a292f09CA2b6D40736a51788E",
         data: "0xd09de08a",
-        value: 0n
-      }
-    ]
+        value: 0n,
+      },
+    ],
   });
 
   console.log(`Your Gelato id is: ${response.id}`);
