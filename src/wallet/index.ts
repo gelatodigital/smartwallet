24cb31a3c200a654d20913d8818d5f74be73e4b9
Brandon Chuah <brandonchuah.sl@gmail.com>
2025-06-19 19:38:06 +0700

    chore: clean up wallet types

--- src/wallet/index.ts
+++ src/wallet/index.ts
@@ -1,12 +1,18 @@
 export enum WalletType {
-  OKX = "okx"
+  Gelato = "gelato",
+  Kernel = "kernel",
+  Safe = "safe",
+  OKX = "okx",
+  TrustWallet = "trustWallet",
+  Custom = "custom"
 }
 
 export enum WalletEncoding {
   ERC7821 = "erc7821",
   Safe = "safe",
   OKX = "okx",
-  ERC7579 = "erc7579"
+  ERC7579 = "erc7579",
+  TrustWallet = "trustWallet"
 }
 
 export interface WalletDetails {
